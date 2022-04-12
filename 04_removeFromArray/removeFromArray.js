3c9a7c4a53c463200827d2797484b0e4370c69ac
Shay Skitel <shay.skitel@gmail.com>
2022-04-13 01:23:51 +0300

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...toRemove) {
+    for (const item of toRemove) {
+        if (arr.indexOf(item) !== -1) {
+            arr.splice(arr.indexOf(item), 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line

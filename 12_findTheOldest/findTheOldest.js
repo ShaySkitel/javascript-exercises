aa8adbc6dd502ecfb63ea8d34f1af0ae6f79efc3
Shay Skitel <shay.skitel@gmail.com>
2022-04-15 02:45:55 +0300

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    const currentDate = new Date().getFullYear();
+    return arr.reduce((previous, next) => {
+        const prevPerson = (previous.yearOfDeath ? previous.yearOfDeath : currentDate) - previous.yearOfBirth;
+        const nextPerson = (next.yearOfDeath ? next.yearOfDeath : currentDate) - next.yearOfBirth;
+        if (prevPerson > nextPerson) return previous;
+        else return next;
+    });
 };
 
 // Do not edit below this line
